be601df6a39a5abd9ad8774b61c1e8b78983d3ff
jacobbogers <jkfbogers@gmail.com>
2022-01-15 04:41:10 +0100

    re-initialize

--- src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-<svelte:head>
-	<title>Svelte Game-of-Life App</title>
-	<link rel="preconnect" href="https://mdn.github.io" crossorigin> 
-</svelte:head>
-<slot></slot>
--- svelte.config.test.cjs
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const preprocess = require('svelte-preprocess');
-
-module.exports = {
-    preprocess: preprocess({})
-};
