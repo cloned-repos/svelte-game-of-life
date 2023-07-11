d2add2be709b58ecbdb6e6b53b178fd5bdb35c51
jacobbogers <info@mail.jacob-bogers.com>
2023-07-11 14:19:52 +0200

    added some doc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,13 @@
 </script>
 
 <div class="wrapper">
+	<!--
+		On Canvas
+		from:
+			https://www.w3.org/TR/2010/WD-html5-20100624/the-canvas-element.html#:~:text=The%20intrinsic%20dimensions%20of%20the,to%20fit%20this%20layout%20size.
+			
+			"The width attribute defaults to 300, and the height attribute defaults to 150."
+	-->
 	<canvas class="me">Alt text</canvas>
 </div>
 
@@ -31,11 +38,28 @@
 		border: 4px solid brown;
 	}
 	.me {
-		min-height: 0; /* --> chrome needs this */
+		min-height: 0; /* --> only chrome needs this (tested on chrome and firefox)  */
 		border: 4px solid yellow;
 		width: 100%;
 		height: 100%;
 		display: block;
 		position: relative;
+		z-index: -1;
+	}
+
+	.skeleton {
+		background-color: #ccc;
+		background-image: linear-gradient(90deg, #ddd 0px, #e8e8e8 40px, #ddd 80px);
+		background-size: 600px;
+		animation: shine-avatar 1.6s infinite linear;
+	}
+	@keyframes shine-lines {
+		0% {
+			background-position: -100px;
+		}
+		40%,
+		100% {
+			background-position: 140px;
+		}
 	}
 </style>
