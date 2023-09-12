cc4b6344eb64096be0438a3f41199794ee94ac5b
jacobbogers <jkfbogers@gmail.com>
2023-09-12 20:38:34 +0200

    bigger canvas for font testbed

--- src/lib/components/Statistics/index.svelte
+++ src/lib/components/Statistics/index.svelte
@@ -32,17 +32,21 @@
 </script>
 
 <div style="--grid-pos: {pos}" class="me">
-	<span
-		>Statistics {$store?.physicalPixelWidth}
-		{$store?.physicalPixelHeight}
-		{(debug('rendering span?'), width)} <span class="fa fa-battery-3" /></span
-	>
+	Statistics
+	<ul>
+		<li>physical-width: {$store?.physicalPixelWidth}</li>
+		<li>physical-height: {$store?.physicalPixelHeight}</li>
+		<li>width: {$store?.width}</li>
+		<li>height: {$store?.height}</li>
+		<li><span class="fa fa-battery-3" /></li>
+		<li>{(debug('rendering ul > li'), width)}</li>
+	</ul>
 	<canvas bind:this={internal} class={$$props.class}> {debug('rendering canvas?')}</canvas>
 </div>
 
 <style>
 	.me {
-		height: 150px;
+		height: 450px;
 		border: 4px darkkhaki dashed;
 		width: 100%;
 		grid-area: var(--grid-pos);
