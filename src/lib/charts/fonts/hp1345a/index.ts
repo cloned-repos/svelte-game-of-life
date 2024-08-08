7340df629b7c45b20919e998a0631d822525b48e
jacobbogers <jkfbogers@gmail.com>
2024-08-08 21:37:33 +0200

    glyph mapped done

--- src/lib/charts/fonts/hp1345a/index.ts
+++ src/lib/charts/fonts/hp1345a/index.ts
@@ -1,29 +1,239 @@
 // source: https://phk.freebsd.dk/hacks/Wargames/
 // all glyphs
-import _1 from './glyphs/01';
-import _2 from './glyphs/02';
-import _4 from './glyphs/04';
-import _5 from './glyphs/05';
-import _6 from './glyphs/06';
-import _7 from './glyphs/07';
-import _14 from './glyphs/0e';
-import _15 from './glyphs/0f';
+import _01 from './glyphs/01';
+import _02 from './glyphs/02';
+
+import _04 from './glyphs/04';
+import _05 from './glyphs/05';
+import _06 from './glyphs/06';
+import _07 from './glyphs/07';
+
+import _0e from './glyphs/0e';
+import _0f from './glyphs/0f';
+import _10 from './glyphs/10';
+import _11 from './glyphs/11';
+import _12 from './glyphs/12';
+import _13 from './glyphs/13';
+import _14 from './glyphs/14';
+import _15 from './glyphs/15';
+import _16 from './glyphs/16';
+import _17 from './glyphs/17';
+import _18 from './glyphs/18';
+import _19 from './glyphs/19';
+import _1a from './glyphs/1a';
+import _1b from './glyphs/1b';
+import _1c from './glyphs/1c';
+import _1d from './glyphs/1d';
+import _1e from './glyphs/1e';
+import _1f from './glyphs/1f';
+
+import _21 from './glyphs/21';
+import _22 from './glyphs/22';
+import _23 from './glyphs/23';
+import _24 from './glyphs/24';
+import _25 from './glyphs/25';
+import _26 from './glyphs/26';
+import _27 from './glyphs/27';
+import _28 from './glyphs/28';
+import _29 from './glyphs/29';
+import _2a from './glyphs/2a';
+import _2b from './glyphs/2b';
+import _2c from './glyphs/2c';
+import _2d from './glyphs/2d';
+import _2e from './glyphs/2e';
+import _2f from './glyphs/2f';
+import _30 from './glyphs/30';
+import _31 from './glyphs/31';
+import _32 from './glyphs/32';
 import _33 from './glyphs/33';
+import _34 from './glyphs/34';
+import _35 from './glyphs/35';
+import _36 from './glyphs/36';
+import _37 from './glyphs/37';
+import _38 from './glyphs/38';
+import _39 from './glyphs/39';
+import _3a from './glyphs/3a';
+import _3b from './glyphs/3b';
+import _3c from './glyphs/3c';
+import _3d from './glyphs/3d';
+import _3e from './glyphs/3e';
+import _3f from './glyphs/3f';
+import _40 from './glyphs/40';
+import _41 from './glyphs/41';
+import _42 from './glyphs/42';
+import _43 from './glyphs/43';
+import _44 from './glyphs/44';
+import _45 from './glyphs/45';
+import _46 from './glyphs/46';
+import _47 from './glyphs/47';
+import _48 from './glyphs/48';
+import _49 from './glyphs/49';
+import _4a from './glyphs/4a';
+import _4b from './glyphs/4b';
+import _4c from './glyphs/4c';
+import _4d from './glyphs/4d';
+import _4e from './glyphs/4e';
+import _4f from './glyphs/4f';
+import _50 from './glyphs/50';
+import _51 from './glyphs/51';
+import _52 from './glyphs/52';
+import _53 from './glyphs/53';
+import _54 from './glyphs/54';
+import _55 from './glyphs/55';
+import _56 from './glyphs/56';
+import _57 from './glyphs/57';
+import _58 from './glyphs/58';
+import _59 from './glyphs/59';
+import _5a from './glyphs/5a';
+import _5b from './glyphs/5b';
+import _5c from './glyphs/5c';
+import _5d from './glyphs/5d';
+import _5e from './glyphs/5e';
+import _5f from './glyphs/5f';
+import _60 from './glyphs/60';
+import _61 from './glyphs/61';
+import _62 from './glyphs/62';
+import _63 from './glyphs/63';
+import _64 from './glyphs/64';
+import _65 from './glyphs/65';
+import _66 from './glyphs/66';
+import _67 from './glyphs/67';
+import _68 from './glyphs/68';
+import _69 from './glyphs/69';
+import _6a from './glyphs/6a';
+import _6b from './glyphs/6b';
+import _6c from './glyphs/6c';
+import _6d from './glyphs/6d';
+import _6e from './glyphs/6e';
+import _6f from './glyphs/6f';
+import _70 from './glyphs/70';
+import _71 from './glyphs/71';
+import _72 from './glyphs/72';
+import _73 from './glyphs/73';
+import _74 from './glyphs/74';
+import _75 from './glyphs/75';
+import _76 from './glyphs/76';
+import _77 from './glyphs/77';
+import _78 from './glyphs/78';
+import _79 from './glyphs/79';
 
 import t from './helpers';
 
 export default function createGlyps() {
     return {
         // hp glyph
-        1: t(_1),
-        // beta
-        2: t(_2),
-        4: t(_4),
-        5: t(_5),
-        6: t(_6),
-        7: t(_7),
-        14: t(_14),
-        15: t(_15),
-        33: t(_33),
+        1: t(_01),
+        2: t(_02),
+        4: t(_04),
+        5: t(_05),
+        6: t(_06),
+        7: t(_07),
+        14: t(_0e),
+        15: t(_0f),
+        16: t(_10),
+        17: t(_11),
+        18: t(_12),
+        19: t(_13),
+        20: t(_14),
+        21: t(_15),
+        22: t(_16),
+        23: t(_17),
+        24: t(_18),
+        25: t(_19),
+        26: t(_1a),
+        27: t(_1b),
+        28: t(_1c),
+        29: t(_1d),
+        30: t(_1e),
+        31: t(_1f),
+        33: t(_21),
+        34: t(_22),
+        35: t(_23),
+        36: t(_24),
+        37: t(_25),
+        38: t(_26),
+        39: t(_27),
+        40: t(_28),
+        41: t(_29),
+        42: t(_2a),
+        43: t(_2b),
+        44: t(_2c),
+        45: t(_2d),
+        46: t(_2e),
+        47: t(_2f),
+        48: t(_30),
+        49: t(_31),
+        50: t(_32),
+        51: t(_33),
+        52: t(_34),
+        53: t(_35),
+        54: t(_36),
+        55: t(_37),
+        56: t(_38),
+        57: t(_39),
+        58: t(_3a),
+        59: t(_3b),
+        60: t(_3c),
+        61: t(_3d),
+        62: t(_3e),
+        63: t(_3f),
+        64: t(_40),
+        65: t(_41),
+        66: t(_42),
+        67: t(_43),
+        68: t(_44),
+        69: t(_45),
+        70: t(_46),
+        71: t(_47),
+        72: t(_48),
+        73: t(_49),
+        74: t(_4a),
+        75: t(_4b),
+        76: t(_4c),
+        77: t(_4d),
+        78: t(_4e),
+        79: t(_4f),
+        80: t(_50),
+        81: t(_51),
+        82: t(_52),
+        83: t(_53),
+        84: t(_54),
+        85: t(_55),
+        86: t(_56),
+        87: t(_57),
+        88: t(_58),
+        89: t(_59),
+        90: t(_5a),
+        91: t(_5b),
+        92: t(_5c),
+        93: t(_5d),
+        94: t(_5e),
+        95: t(_5f),
+        96: t(_60),
+        97: t(_61),
+        98: t(_62),
+        99: t(_63),
+        100: t(_64),
+        101: t(_65),
+        102: t(_66),
+        103: t(_67),
+        104: t(_68),
+        105: t(_69),
+        106: t(_6a),
+        107: t(_6b),
+        108: t(_6c),
+        109: t(_6d),
+        110: t(_6e),
+        111: t(_6f),
+        112: t(_70),
+        113: t(_71),
+        114: t(_72),
+        115: t(_73),
+        116: t(_74),
+        117: t(_75),
+        118: t(_76),
+        119: t(_77),
+        120: t(_78),
+        121: t(_79),
     };
 }
